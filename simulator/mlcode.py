45678fa940258a874ad057c5569482d7a68ff9d2
/./ <eugeneangjy7@gmail.com>
2022-11-18 08:51:57 +0800

    a

--- simulator/mlcode.py
+++ simulator/mlcode.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import fft, ifft, fftshift, ifftshift
 
-from main import data
+from main import test_data
 #before modifying the code for the ml, need to know the number of units and stuff
 """Rv map size is 124,416 pixels large"""
 ## TODO
